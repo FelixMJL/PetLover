18244f35043ec8783baf5a332db75b08834fc873
WeidongFang1985 <fangweidong1985@gmail.com>
2023-02-25 20:16:40 +1100

    Recommend for you -- underline centered fixed

--- src/components/Header/Follow/Follow.js
+++ src/components/Header/Follow/Follow.js
@@ -3,7 +3,7 @@ import "./Follow.css";
 const Follow = () => {
     return (
         <div className="follow">
-            <button className="follow__item follow__item-active">Recommend for you</button>
+            <button className="follow__item follow__recommend follow__recommend-active">Recommend for you</button>
             <button className="follow__item ">Following</button>
         </div>
     );
