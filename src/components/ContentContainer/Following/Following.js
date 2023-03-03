88334883121b4067c8c379efe7bbb9b8d05bb0cb
one1203 <15502906281@163.com>
2023-03-03 18:48:06 +1000

    Show Following Posts

--- src/components/ContentContainer/Following/Following.js
+++ src/components/ContentContainer/Following/Following.js
@@ -1,27 +1,60 @@
 import "./Following.css"
+import {getFollowing} from "../../../services/getFollowing";
+import { useState, useEffect } from "react";
+import moment from 'moment';
 
 const Following = () => {
+    const [postData, setUserData] = useState(0)
+    useEffect(() => {
+        getFollowing().then(user => setUserData(user.data))
+    }, [])
+
+    function DataList({ posts }) {
+        const list = posts.map((post,i) => 
+    <li key={`${i}`}>
+        <div class="post-container">
+            <div class="post-inner-container">
+                <div class="avatar">
+                    <img src={post.author.avatar} alt=""/>
+                </div>
+                <div class="post-content-container">
+                    <div class="post-info">
+                        <div class="nickname">
+                            <p><b>{post.author.nickname}</b></p>
+                        </div>
+                        <div class="username">
+                            <p>@{post.author.username}</p>
+                        </div>
+                        <div class="dot"> · </div>
+                        <div class="post-time">
+                            <p>{moment(post.created_at).fromNow()}</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p>{post.content}</p>
+                    </div>
+                    <div class="post-pic">
+                        <img src={post.photo} alt=""/>
+                    </div>
+                    <div class="comment">
+                        <img src="" alt=""></img>
+                        <p class="comment-count">{post.comments.length}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </li>
+    );
+        return <ul>{list}</ul>;
+      }
+
     return (
         <div>
             <h1>Following</h1>
-            <h2>红包🧧拿来1</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
-            <h2>红包🧧拿来</h2>
+            <div>
+                <DataList posts={Array.from(postData)} />
+            </div>
+            <div class="foot-space"></div>
         </div>
     );
 };
---
+++ src/services/getFollowing.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const getFollowing = async () => {
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmM4YjA0MGZkN2Q4NWU0M2E3ZTBmMyIsImVtYWlsIjoid2FuZ0BnbWFpbC5jb20iLCJpYXQiOjE2Nzc4MjkzMTAsImV4cCI6MTY3NzkxNTcxMH0.6tEz1I5Uxsi4Gz50Dz5ssCDUd0Q418kSUzy0UIrCn64"
+  const config = {
+    headers: {Authorization: `Bearer ${token}`}
+  }
+  return await axios.get("http://localhost:3000/api/v1//posts/users/63fc8b040fd7d85e43a7e0f3", config)
+}
