6916edb76d042873849c1cdb3306cc691bd29cfb
WeidongFang1985 <fangweidong1985@gmail.com>
2023-03-09 23:23:15 +1100

    add eslint

--- src/views/Following.jsx
+++ src/views/Following.jsx
@@ -1,20 +1,23 @@
-import Header from "../components/Header/Header";
-import {Link} from "react-router-dom";
-import Footer from "../components/Footer/Footer";
-import FollowingUsersPosts from "../components/FollowingUsersPosts/FollowingUsersPosts";
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import FollowingUsersPosts from '../components/FollowingUsersPosts/FollowingUsersPosts';
 
-const Following = () => {
-    return (
-        <div>
-            <Header/>
-            <div className="recommend">
-                <Link className="item" to="/">Recommend for you</Link>
-                <Link className="item" to="/following">Following</Link>
-            </div>
-            <FollowingUsersPosts />
-            <Footer/>
-        </div>
-    );
-};
+const Following = () => (
+  <div>
+    <Header />
+    <div className="recommend">
+      <Link className="item" to="/">
+        Recommend for you
+      </Link>
+      <Link className="item" to="/following">
+        Following
+      </Link>
+    </div>
+    <FollowingUsersPosts />
+    <Footer />
+  </div>
+);
 
 export default Following;
