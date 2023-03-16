4fc42fae8cc9aa59bd1923baaf3ed4a97bae66f5
Teng Liu <ericliu940430@gmail.com>
2023-03-17 09:23:06 +1100

    Pl-78 done by ERIC 02

--- src/components/UserProfile/post/UserPost.jsx
+++ src/components/UserProfile/post/UserPost.jsx
@@ -1,6 +1,6 @@
 import '../../RecommendForYou/post/post.css';
-import { FaRegComment } from 'react-icons/fa';
 import React from 'react';
+import replyLogo from '../../../assets/reply.png';
 
 const UserPost = ({ content, photo, comments }) => (
   <div className="post_content-container">
@@ -8,7 +8,7 @@ const UserPost = ({ content, photo, comments }) => (
       <div className="post_content-text">{content}</div>
       <img src={photo} className="post_content-image" alt="content-img" />
       <div className="post_comments">
-        <FaRegComment />
+        <img src={replyLogo} alt="replyLogo" className="post_comments-replyLogo" />
         <span className="post_comments-count">{comments.length}</span>
       </div>
     </div>
