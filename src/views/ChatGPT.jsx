cba2b33f2bf1e46304207ddd8f0bd8ba2b606af4
WeidongFang1985 <fangweidong1985@gmail.com>
2023-03-26 19:39:30 +1100

    chatgpt done with styling

--- src/views/ChatGPT.jsx
+++ src/views/ChatGPT.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Footer from '../components/Footer/Footer';
+import { getUserData } from '../services/getUserData';
 
 const ChatGPT = () => {
-  let e;
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+  const textareaChangeHandler = (e) => {
+    setResult(e.target.value);
+  };
+  const chatGpt = async () => {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_ENDPOINT}/api/v1/openai/chatGpt`,
+      { question: input },
+      getUserData().config,
+    );
+    setResult(response.data);
+  };
+
   return (
     <div>
-      <h1>chatgpt {e}</h1>
-      <p>Hello world</p>
+      <h1>chatgpt</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={inputChangeHandler}
+        placeholder="Please input your question"
+      />
+      <br />
+      <br />
+      <button type="button" onClick={chatGpt}>
+        Submit
+      </button>
+      <br />
+      <br />
+      <textarea
+        value={result}
+        style={{ width: '500px' }}
+        rows="8"
+        onChange={textareaChangeHandler}
+      />
       <Footer />
     </div>
   );
