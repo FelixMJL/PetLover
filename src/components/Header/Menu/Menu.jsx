618f159d2e6af7c1ae7b463703edfbc668a735aa
WeidongFang1985 <fangweidong1985@gmail.com>
2023-04-14 00:12:10 +1000

    add links to menu

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -6,6 +6,8 @@ import logoutIcon from '../../../assets/icon-logout.png';
 import chatGpt from '../../../assets/chatGPT.svg';
 import imageGeneration from '../../../assets/openai-image.svg';
 import team from '../../../assets/team.svg';
+import privacy from '../../../assets/privacy.svg';
+import terms from '../../../assets/terms.svg';
 
 const Menu = ({ profileMenu, closeMenu, userData, userId, darkBackground }) => {
   const navigate = useNavigate();
@@ -64,6 +66,14 @@ const Menu = ({ profileMenu, closeMenu, userData, userId, darkBackground }) => {
             Image Generation
           </Link>
 
+          <Link className="user-menu__logoutBtn terms" to="/terms">
+            <img src={terms} alt="terms and conditions" />
+            Terms and Conditions
+          </Link>
+          <Link className="user-menu__logoutBtn privacy" to="/privacy">
+            <img src={privacy} alt="privacy policy" />
+            Privacy Policy
+          </Link>
           <Link className="user-menu__logoutBtn team" to="/team">
             <img src={team} alt="our tem" />
             About Our Team
